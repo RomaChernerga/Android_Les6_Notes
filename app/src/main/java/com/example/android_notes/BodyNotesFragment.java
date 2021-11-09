94efc7712b3e8dbc2968814ef47b275d90220d6f
Roman Chernega <roman-chernega@yandex.ru>
2021-11-09 19:49:43 +0300

    Android_Lesson6_Notes_Update

--- app/src/main/java/com/example/android_notes/BodyNotesFragment.java
+++ app/src/main/java/com/example/android_notes/BodyNotesFragment.java
@@ -8,11 +8,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.renderscript.Sampler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class BodyNotesFragment extends Fragment {
 
@@ -49,6 +51,9 @@ public class BodyNotesFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        TextView textView = view.findViewById(R.id. note_name);
+
 
         EditText editText = view.findViewById(R.id.body_note);
         editText.setBackgroundColor(Color.WHITE);
